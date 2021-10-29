683056a9895be430a63f1b515ac3c0aedf5dd89f
lgustavogdc <lgustavogdc@gmail.com>
2021-10-28 21:24:25 -0300

    feat: add dynamic base url

--- src/services/api/AxiosAdapter.ts
+++ src/services/api/AxiosAdapter.ts
@@ -2,15 +2,13 @@ import axios from 'axios'
 import { HttpGet, HttpRequest, HttpResponse } from 'src/services/protocols/api'
 
 export class AxiosAdapterGet<ResponseBody = any> implements HttpGet<ResponseBody> {
+  constructor(private readonly baseUrl: string) {}
+
   async get(getParams: Omit<HttpRequest, 'body'>): Promise<HttpResponse<ResponseBody>> {
-    const response = await axios.get<ResponseBody>('http://localhost:3000' + getParams.url, { params: getParams.params })
+    const response = await axios.get<ResponseBody>(this.baseUrl + getParams.url, { params: getParams.params })
     return {
       statusCode: response.status,
       body: response.data,
     }
   }
-}
-
-export default {
-  get: AxiosAdapterGet
 }
