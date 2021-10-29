892ec1a944de7385d048df33c0fea6de2bfabe91
lgustavogdc <lgustavogdc@gmail.com>
2021-10-28 21:23:48 -0300

    feat: create user model

---
+++ src/models/github/user.ts
@@ -0,0 +1,12 @@
+export type GithubUser = {
+  login: string
+  id: number
+  avatar_url: string
+  html_url: string
+  repos_url: string
+  name: string
+  location: string
+  email: string
+  bio: string
+  public_repos: number
+}
