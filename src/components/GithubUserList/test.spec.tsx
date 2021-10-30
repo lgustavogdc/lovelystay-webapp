e81625eb4dc1e414b5aafa323983590d7073565c
lgustavogdc <lgustavogdc@gmail.com>
2021-10-30 18:30:01 -0300

    test: create GithubUserList component

---
+++ src/components/GithubUserList/test.spec.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import GithubUserList, { GithubUserListProps } from '.'
+
+const mockedGithubUserListProps: GithubUserListProps = {
+  users: [{ id: 1, login: 'anyLogin', name: 'anyName', email: 'anyEmail', avatar_url: 'anyUrl' }],
+  onSelectUser: jest.fn(),
+}
+describe('<GithubUserList />', () => {
+  it('should render the githubuserlist', () => {
+    render(<GithubUserList {...mockedGithubUserListProps} />)
+    const userList = screen.getByTestId('ListWrapper')
+
+    expect(userList).toBeInTheDocument()
+  })
+
+  it('should call onSelectUser after clicking component', () => {
+    const mockedOnSelectUser = jest.fn()
+    render(<GithubUserList {...mockedGithubUserListProps} onSelectUser={mockedOnSelectUser} />)
+    const user = screen.getByText('anyName').parentElement!
+
+    userEvent.click(user)
+
+    expect(mockedOnSelectUser).toBeCalledTimes(1)
+  })
+
+  it('should have the same children amount as users array length', () => {
+    render(<GithubUserList {...mockedGithubUserListProps} />)
+    const userList = screen.getByTestId('ListWrapper')
+
+    expect(userList.childElementCount).toBe(1)
+  })
+})
