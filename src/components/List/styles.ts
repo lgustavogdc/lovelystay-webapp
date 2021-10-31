9253cad7d7c6b5d663fe448b229b9c9d6062a473
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 17:58:01 -0300

    feat: add grid list style

--- src/components/List/styles.ts
+++ src/components/List/styles.ts
@@ -1,6 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { CustomStyle } from '.'
 
 export const Wrapper = styled.div.attrs({
   'data-testid': 'ListWrapper',
-})<{ customStyle: CustomStyle }>``
+})<{ customStyle: CustomStyle }>`
+  ${({ customStyle }) => css`
+    ${customStyle === 'grid' &&
+    css`
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 10px;
+    `}
+  `}
+`
