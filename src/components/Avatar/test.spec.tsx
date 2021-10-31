b0012d99e1620af794b64c1fda72c17d5075696d
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 16:10:27 -0300

    test: style avatar component

--- src/components/Avatar/test.spec.tsx
+++ src/components/Avatar/test.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-
+import 'jest-styled-components'
 import Avatar, { AvatarProps } from '.'
 
 const mockedAvatarProps: AvatarProps = {
@@ -13,5 +13,13 @@ describe('<Avatar />', () => {
     const avatar = screen.getByTestId('Avatar')
 
     expect(avatar).toBeInTheDocument()
+  })
+
+  it('should have a different style depending on custom styleProp', () => {
+    render(<Avatar {...mockedAvatarProps} customStyle="large" />)
+    const avatar = screen.getByTestId('Avatar')
+
+    expect(avatar).toHaveStyleRule('height', '260px')
+    expect(avatar).toHaveStyleRule('width', '260px')
   })
 })
