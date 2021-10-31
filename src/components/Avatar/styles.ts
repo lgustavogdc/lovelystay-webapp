f62e85823e6d859325248e3dc41f751b89c38de8
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 16:05:20 -0300

    feat: style avatar component

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,6 +1,24 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { AvatarStyle } from '.'
 
 export const Avatar = styled.img.attrs({
   'data-testid': 'Avatar',
-})<{ customStyle: AvatarStyle }>``
+})<{ customStyle: AvatarStyle }>`
+  ${({ customStyle }) => css`
+    border-radius: 50%;
+
+    ${customStyle &&
+    customStyle === 'small' &&
+    css`
+      height: 20px;
+      width: 20px;
+    `}
+
+    ${customStyle &&
+    customStyle === 'large' &&
+    css`
+      height: 260px;
+      width: 260px;
+    `}
+  `}
+`
