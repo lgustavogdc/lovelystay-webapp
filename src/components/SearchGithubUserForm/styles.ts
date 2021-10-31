631e46a9117c82001139443cc20c2d13e6463a42
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 16:55:03 -0300

    feat: style search github user form

--- src/components/SearchGithubUserForm/styles.ts
+++ src/components/SearchGithubUserForm/styles.ts
@@ -3,14 +3,26 @@ import Button from '../Button'
 
 export const Wrapper = styled.div.attrs({
   'data-testid': 'SearchGithubUserFormWrapper',
-})``
+})`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
-export const ButtonsWrapper = styled.div``
+export const ButtonsWrapper = styled.div`
+  margin-top: 10px;
+  width: 250px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 export const SearchUsersButton = styled(Button).attrs({
-  style: 'primary',
+  customStyle: 'primary',
 })``
 
 export const SearchUserButton = styled(Button).attrs({
-  style: 'secondary',
+  customStyle: 'secondary',
 })``
