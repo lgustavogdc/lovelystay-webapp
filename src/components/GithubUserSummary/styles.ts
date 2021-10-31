9dc6cd2ef8dabd753e189ded161f232764caf089
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 19:13:55 -0300

    feat: style github user summary component

--- src/components/GithubUserSummary/styles.ts
+++ src/components/GithubUserSummary/styles.ts
@@ -2,10 +2,27 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div.attrs({
   'data-testid': 'GithubUserSummaryWrapper',
-})``
+})`
+  background-color: #21212d;
+  border-radius: 8px;
+  margin: 10px 0;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  color: #005efe;
+  cursor: pointer;
+`
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  margin: 0;
+  margin-left: 10px;
+  font-size: 20px;
+`
 
-export const SubtitleWrapper = styled.div``
-
-export const Subtitle = styled.h2``
+export const Subtitle = styled.h2.attrs({
+  'data-testid': 'GithubUserSummarySubtitle',
+})`
+  margin: 0;
+  margin-left: 10px;
+  font-size: 18px;
+`
