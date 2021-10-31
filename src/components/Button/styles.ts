4ee9ea2020140fdd8c550a61d3b19fc4f79128ad
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 16:38:28 -0300

    feat: style button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,26 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { ButtonStyle } from '.'
 
 export const Button = styled.button.attrs({
   'data-testid': 'Button',
-})<{ customStyle: ButtonStyle }>``
+})<{ customStyle: ButtonStyle }>`
+  border: none;
+  border-radius: 8px;
+  padding: 10px;
+  text-decoration: none;
+  text-align: center;
+
+  ${({ customStyle }) => css`
+    ${customStyle === 'primary' &&
+    css`
+      color: #fff;
+      background-color: #005efe;
+    `}
+
+    ${customStyle === 'secondary' &&
+    css`
+      color: #2c5fbc;
+      background-color: #222a3f;
+    `}
+  `}
+`
