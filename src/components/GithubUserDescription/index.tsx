e9016524efda96c92c98ead7d2771a40c20966c4
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 23:25:24 -0300

    refactor: add number of repositories text

--- src/components/GithubUserDescription/index.tsx
+++ src/components/GithubUserDescription/index.tsx
@@ -3,14 +3,14 @@ import * as S from './styles'
 export type GithubUserDescriptionProps = {
   name: string
   username: string
-  totalRepositories: number
+  totalRepositories: number | null
 }
 
 const GithubUserDescription = ({ name, username, totalRepositories }: GithubUserDescriptionProps) => (
   <S.Wrapper>
     <S.Username>{username}</S.Username>
     <S.Name>{name}</S.Name>
-    <S.TotalRepositories>{totalRepositories}</S.TotalRepositories>
+    <S.TotalRepositories>Number of repositories: {totalRepositories}</S.TotalRepositories>
   </S.Wrapper>
 )
 
