3f919bc019cb66e5dc5f558fe0eee6c400b785a5
lgustavogdc <lgustavogdc@gmail.com>
2021-11-01 11:21:34 -0300

    feat: implement loading on User page

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -4,10 +4,12 @@ import RepositoryList from 'src/components/RepositoryList'
 import { GithubUser } from 'src/models/github'
 import { Repository } from 'src/models/github/repository'
 import { createGithubService } from 'src/services'
+import Loading from 'src/components/Loading'
 import * as S from './styles'
 
 const User = () => {
   const username = window.location.pathname.replace('/user/', '')
+  const [isLoading, setIsLoading] = useState(false)
   const [userRepositories, setUserRepositories] = useState<Repository[]>([])
   const [user, setUser] = useState<Pick<GithubUser, 'id' | 'login' | 'name' | 'avatar_url' | 'public_repos'>>({
     login: '',
@@ -19,6 +21,7 @@ const User = () => {
 
   useEffect(() => {
     try {
+      setIsLoading(true)
       const githubService = createGithubService()
       const getUser = async () => {
         const specificUser = await githubService.getUser(username)
@@ -28,24 +31,32 @@ const User = () => {
       const getRepositories = async () => {
         const repositories = await githubService.getGithubUserRepositories(`/users/${username}/repos`)
         setUserRepositories(repositories)
+        setIsLoading(false)
       }
 
       getUser()
       getRepositories()
     } catch (error) {
       console.log(error)
+      setIsLoading(false)
     }
   }, [username])
 
   return (
     <S.Wrapper>
-      <GithubUserInfo
-        avatar={user.avatar_url}
-        name={user.name}
-        username={user.login}
-        totalRepositories={user.public_repos}
-      />
-      <RepositoryList repositories={userRepositories} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <S.InfoWrapper>
+          <GithubUserInfo
+            avatar={user.avatar_url}
+            name={user.name}
+            username={user.login}
+            totalRepositories={user.public_repos}
+          />
+          <RepositoryList repositories={userRepositories} />
+        </S.InfoWrapper>
+      )}
     </S.Wrapper>
   )
 }
--- src/pages/User/styles.ts
+++ src/pages/User/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.main.attrs({
   'data-testid': 'UserWrapper',
-})`
+})``
+
+export const InfoWrapper = styled.div`
   margin-top: 20px;
   display: flex;
   justify-content: space-around;
