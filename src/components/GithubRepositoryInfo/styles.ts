4b8ea386e3815f524aba1ce39338187015308a28
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 23:24:53 -0300

    feat: style github repository info

--- src/components/GithubRepositoryInfo/styles.ts
+++ src/components/GithubRepositoryInfo/styles.ts
@@ -2,10 +2,27 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div.attrs({
   'data-testid': 'GithubRepositoryInfoWrapper',
-})``
+})`
+  border: 2px solid #30363d;
+  border-radius: 10px;
+  padding: 5px;
+  width: 100%;
+  background-color: #21212d;
+`
 
-export const RepositoryName = styled.h1``
+export const RepositoryName = styled.h1`
+  color: #58a6ff;
+  font-weight: 600;
+  font-size: 14px;
+  padding: 10px;
+`
 
 export const RepositoryDescription = styled.p.attrs({
   'data-testid': 'GithubRepositoryDescription',
-})``
+})`
+  margin-top: 8px;
+  margin-bottom: 16px;
+  font-size: 12px;
+  color: #8b949e;
+  padding: 5px;
+`
