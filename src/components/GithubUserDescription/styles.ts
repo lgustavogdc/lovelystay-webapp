76561c5b431d27e89f12ae690c791b89a65c6470
lgustavogdc <lgustavogdc@gmail.com>
2021-10-31 23:31:06 -0300

    feat: style Github user description component

--- src/components/GithubUserDescription/styles.ts
+++ src/components/GithubUserDescription/styles.ts
@@ -2,10 +2,19 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div.attrs({
   'data-testid': 'GithubUserDescriptionWrapper',
-})``
+})`
+  border-bottom: 2px solid #30363d;
+  color: #8b949e;
+  font-weight: 300;
+  line-height: 20px;
+`
 
-export const Username = styled.h2``
+export const Username = styled.h2`
+  font-size: 20px;
+`
 
-export const Name = styled.p``
+export const Name = styled.p`
+  font-size: 18px;
+`
 
 export const TotalRepositories = styled.p``
