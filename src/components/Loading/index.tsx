d9ec7b3c741ad33864f123c52eec034cdefdda84
lgustavogdc <lgustavogdc@gmail.com>
2021-11-01 09:45:32 -0300

    feat: create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const Loading = () => (
+  <S.LoadingWrapper>
+    <CircularProgress />
+  </S.LoadingWrapper>
+)
+
+export default Loading
