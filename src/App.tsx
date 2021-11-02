625c6010c6e298e7e9bf5afc3e38b8d136fcf1de
lgustavogdc <lgustavogdc@gmail.com>
2021-11-01 21:29:40 -0300

    feat: implement pages on app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import './App.css';
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import Home from './pages/Home'
+import theme from './styles/theme'
+import { BrowserRouter as Switch, Route } from 'react-router-dom'
+import User from './pages/User'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/user/:login">
+          <User />
+        </Route>
+      </Switch>
     </ThemeProvider>
-    
-  );
+  )
 }
 
-export default App;
+export default App
